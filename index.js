9e9aae857f4c490736c4ee34d5cd58b9bcc35c11
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-11-18 21:29:29 -0500

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+divide (2000,100);
+
+const square = (number) => {
+    return number * number;
+}
+square(4)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(20,50);
